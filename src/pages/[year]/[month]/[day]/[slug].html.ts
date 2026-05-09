682cd61b972887d645ed93a573fa3743937b1eed
Vibhoothi <vbhthi@tcd.ie>
2026-05-09 14:18:28 +0100

    src: More migration
    This adds redirects, brings back old logo (for-now), analytics,
    more links, and other minor tweaks

---
+++ src/pages/[year]/[month]/[day]/[slug].html.ts
@@ -0,0 +1,21 @@
+import { getCollection } from 'astro:content';
+import { getBlogSlug, getLegacyBlogParams } from '../../../../utils/routes';
+import { renderRedirectHtml } from '../../../../utils/redirect';
+
+export async function getStaticPaths() {
+  const posts = await getCollection('posts');
+  return posts.flatMap((entry) => getLegacyBlogParams(entry.id, entry.data.categories)
+    .filter((params) => !params.category)
+    .map((params) => ({
+      params,
+      props: { target: `${import.meta.env.BASE_URL}blog/${getBlogSlug(entry.id)}` },
+    })));
+}
+
+export async function GET({ props }: { props: { target: string } }) {
+  return new Response(renderRedirectHtml(props.target), {
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+  });
+}
